4fe391c62e2a1efc41ace77d428519ef37727fa0
Tomas Avendaño Soriano <toavendano.dwfsc5@bootcampudd.cl>
2023-06-10 09:32:08 -0400

    camibos en user model

--- src/resources/users/models/user.model.js
+++ src/resources/users/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+// forma de forzar la importacion para error COMMONG JS - import * validator from "validator"
 
 // const userSchema1 = new mongoose.Schema( {
 //   name: String,
