33db2e5671e2ab231250c53954a17d571db74bed
Tomas Avendaño Soriano <toavendano.dwfsc5@bootcampudd.cl>
2023-07-14 19:44:49 -0400

    import products router

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import express from 'express'
 import { startConnection } from './config/database.js'
 import environment from './config/environment.js'
 import usersRouter from './resources/users/routes/users.routes.js'
-import postsRouter from './resources/posts/routes/posts.routes.js'
 import authRoutes from './resources/auth/routes/auth.routes.js'
+import productsRouter from './resources/products/routes/products.routes.js'
 
 const app = express()
 
@@ -18,7 +18,7 @@ app.get( '/', function ( req, res ) {
 } )
 
 app.use( usersRouter )
-app.use( postsRouter )
+app.use( productsRouter )
 app.use( authRoutes )
 
 const { PORT } = environment
